56d9dd55e9e949da6ba7eecf1ee1cfd5a81ad94f
rsalidosanchez <rsalidosanchez@ITEM-66607.emea.msad.sopra>
2018-04-10 10:59:01 +0200

    Commit inicial

---
+++ PruebaCRUD/src/main/java/com/crud/PruebaCrudApplication.java
@@ -0,0 +1,12 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCrudApplication.class, args);
+	}
+}
